9b67e97372727fb2a2bfc04b1a2e943304e7f959
Chris G Liu <59263349+chrisgliu@users.noreply.github.com>
2024-10-23 17:54:27 -0400

    compare

---
+++ compareDistance.py
@@ -0,0 +1,64 @@
+import csv
+import json
+from geopy.distance import geodesic
+
+actual = {}
+with open('result.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for line in csvFile:
+        file = line[0].split("\\")[-1]
+        lat = line[1]
+        long = line[2]
+        actual[file] = [lat, long]
+
+picarta = {}
+with open('output.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for line in csvFile:
+        file = line[0]
+        lat = line[1]
+        long = line[2]
+        picarta[file] = [lat, long]
+
+google = {}
+with open('strippedresult.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for line in csvFile:
+        file = line[0].split("\\")[-1]
+        lat = line[1]
+        long = line[2]
+        google[file] = [lat, long]
+
+output = {}
+
+for image in actual.keys():
+    lat = actual[image][0]
+    long = actual[image][1]
+    target = (f"{lat},{long}")
+
+    plat = picarta[image][0]
+    plong = picarta[image][1]
+    pguess =  (f"{plat},{plong}")
+
+    pmiles = geodesic(target, pguess).miles
+
+    glat = google[image][0]
+    glong = google[image][1]
+    gmiles = -1
+    if glat != "NULL" or glong != "NULL":
+        gguess =  (f"{glat},{glong}")
+        gmiles = geodesic(target, gguess).miles
+
+    data = {}
+    data["picarta"] = f"{pmiles:.2f} miles"
+    data["google"] = f"{gmiles:.2f} miles"
+    output[image] = data
+
+# Serializing json
+json_output = json.dumps(output, indent=4)
+ 
+# Writing to json file
+with open("compareDistance.json", "w") as outfile:
+    outfile.write(json_output)
+
+
