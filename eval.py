fb4855c5c71bc4b8ad2c58d03f990d15f2c85a1b
Spongebanana27 <nsevans2707@gmail.com>
2024-11-14 16:46:14 -0500

    image

--- eval.py
+++ eval.py
@@ -19,11 +19,20 @@ pbest = -1
 gbest = -1
 cbest = -1
 
+pbestimg = ""
+gbestimg = ""
+cbestimg = ""
+
 pworst = 10000000
 gworst = 10000000
 cworst = 10000000
 
-for image in data.values():
+pworstimg = ""
+gworstimg = ""
+cworstimg = ""
+
+
+for key, image in data.items():
     pdistance = image["picarta"].strip().replace("miles","")
     gdistance = image["google"].strip().replace("miles","")
     cdistance = image["chatgpt"].strip().replace("miles","")
@@ -33,22 +42,28 @@ for image in data.values():
         ptotal = ptotal + float(pdistance)
         if pbest == -1 or pbest > float(pdistance):
             pbest = float(pdistance)
+            pbestimg = key
         if pworst == 10000000 or pworst < float(pdistance):
             pworst = float(pdistance)
+            pworstimg = key
     if "-1" not in gdistance:
         gguesses = gguesses + 1
         gtotal = gtotal + float(gdistance)
         if gbest == -1 or gbest > float(gdistance):
             gbest = float(gdistance)
+            gbestimg = key
         if gworst == 10000000 or gworst < float(pdistance):
             gworst = float(pdistance)
+            gworstimg = key
     if "-1" not in cdistance:
         cguesses = cguesses + 1
         ctotal = ctotal + float(cdistance)  
         if cbest == -1 or cbest > float(cdistance):
             cbest = float(cdistance) 
+            cbestimg = key
         if cworst == 10000000 or cworst < float(pdistance):
             cworst = float(pdistance)
+            cworstimg = key
         
 print("Number of images tested")
 print(nimages)
@@ -65,21 +80,27 @@ cavg = ctotal / cguesses
 
 print("Lowest error distance for Picarta")
 print("{0:.2f} miles".format(pbest))
+print("On image: " + pbestimg)
 print("Lowest error distance for Google")
 print("{0:.2f} miles".format(gbest))
+print("On image: " + gbestimg)
 print("Lowest error distance for ChatGPT")
-print("{0:.2f} miles\n".format(cbest))
+print("{0:.2f} miles".format(cbest))
+print("On image: " + cbestimg)
 
-print("Highest error distance for Picarta")
+print("\nHighest error distance for Picarta")
 print("{0:.2f} miles".format(pworst))
+print("On image: " + pworstimg)
 print("Highest error distance for Google")
 print("{0:.2f} miles".format(gworst))
+print("On image: " + gworstimg)
 print("Highest error distance for ChatGPT")
-print("{0:.2f} miles\n".format(cworst))
+print("{0:.2f} miles".format(cworst))
+print("On image: " + cworstimg)
 
-print("Average error distance for Picarta")
+print("\nAverage error distance for Picarta")
 print("{0:.2f} miles".format(pavg))
 print("Average error distance for Google")
 print("{0:.2f} miles".format(gavg))
 print("Average error distance for ChatGPT")
-print("{0:.2f} miles\n".format(cavg))
+print("{0:.2f} miles".format(cavg))
